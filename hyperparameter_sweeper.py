822b6b2870ed27d212f85b349009d43fe36ecccd
sathishkumar67 <sathishvillers2000@gmail.com>
2024-10-05 14:28:29 +0530

    changed hyperparameter values and no of parameter choosing=10

--- hyperparameter_sweeper.py
+++ hyperparameter_sweeper.py
@@ -57,15 +57,14 @@ if __name__ == "__main__":
 
     # Your hyperparameters
     hyperparameters = {
-        "alpha_beta": [(0.1, 0.9), (0.2, 0.8), (0.3, 0.7), (0.4, 0.6), (0.5, 0.5), (0.6, 0.4), (0.7, 0.3), (0.8, 0.2), (0.9, 0.1)],  # alpha and beta for the loss function
-        "lr": np.linspace(3e-4, 3e-5, 9),  # learning rate
-        "weight_decay": np.linspace(0.1, 0.00001, 9),  # weight decay
-        "betas": [(0.9, 0.999), (0.85, 0.999), (0.9, 0.99), (0.92, 0.998), (0.95, 0.9995), (0.9, 0.9999), (0.8, 0.999), (0.85, 0.995), (0.88, 0.9985)],  # betas for optimizer
-        "epochs": [5, 10, 15, 20, 25, 30, 35, 40, 45]  # epochs
+        "alpha_beta": [(0.4, 0.6), (0.5, 0.5), (0.6, 0.4)],  # alpha and beta for the loss function
+        "lr": [1e-3, 1e-4, 1e-5],  # learning rate
+        "weight_decay": [1e-3, 1e-4, 1e-5],  # weight decay
+        "epochs": [5, 10, 15]  # epochs
     }
 
     # Generate 20 random hyperparameter combinations
-    random_combinations = list(ParameterSampler(hyperparameters, n_iter=20, random_state=42))
+    random_combinations = list(ParameterSampler(hyperparameters, n_iter=10, random_state=1337))
 
     # Random search for find best hyperparameters
     def random_search(hyperparameters):
